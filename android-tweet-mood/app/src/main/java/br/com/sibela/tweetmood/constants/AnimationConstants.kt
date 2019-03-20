f90d9ee249b216cb192353925ebed78bd8d441f1
leonardosibela <leonardo_sibela@hotmail.com>
2019-03-20 19:29:13 -0300

    Animating tweets recyclerview

--- android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/constants/AnimationConstants.kt
+++ android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/constants/AnimationConstants.kt
@@ -6,5 +6,6 @@ class AnimationConstants {
         const val CONSTRAINT_SET_INTERMEDIATE_DURATION = 1000L
         const val ANTICIPATE_OVERSHOOT_INTERPOLATOR_INTERMEDIATE_TENSION = 1.0f
         const val CONSTRAINT_SET_STARTING_DELAY = 1000L
+        const val DISPLAY_TWEETS_DELAY = 500L
     }
 }
--- android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/view/TweetsListActivity.kt
+++ android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/view/TweetsListActivity.kt
@@ -1,10 +1,13 @@
 package br.com.sibela.tweetmood.view
 
 import android.os.Bundle
+import android.os.Handler
 import android.support.v7.app.AppCompatActivity
 import android.support.v7.widget.LinearLayoutManager
+import android.view.animation.AnimationUtils
 import br.com.sibela.tweetmood.R
 import br.com.sibela.tweetmood.adapter.TweetsAdapter
+import br.com.sibela.tweetmood.constants.AnimationConstants
 import br.com.sibela.tweetmood.model.Tweet
 import kotlinx.android.synthetic.main.activity_tweets_list.*
 
@@ -17,11 +20,14 @@ class TweetsListActivity : AppCompatActivity(), TweetsAdapter.Callback {
         setContentView(R.layout.activity_tweets_list)
 
         val tweets: ArrayList<Tweet> = intent.getParcelableArrayListExtra(TWEETS_DATA_KEY)
-        setupRecycler(tweets)
+        Handler().postDelayed({
+            setupRecycler(tweets)
+        }, AnimationConstants.DISPLAY_TWEETS_DELAY)
     }
 
     private fun setupRecycler(tweets: ArrayList<Tweet>) {
         tweetsRecycler.setHasFixedSize(true)
+        tweetsRecycler.layoutAnimation = AnimationUtils.loadLayoutAnimation(this, R.anim.layout_animation_fall_down)
         tweetsRecycler.layoutManager = LinearLayoutManager(tweetsRecycler.context)
         tweetsRecycler.addItemDecoration(CardSpacingItemDecoration(this, R.dimen.default_margin_between_elements))
         tweetsAdapter = TweetsAdapter(tweets, this)
