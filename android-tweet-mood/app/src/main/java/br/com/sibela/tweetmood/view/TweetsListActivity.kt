e7411544b7cf34b1ec7a8ffa9edd17dd842cad87
leonardosibela <leonardo_sibela@hotmail.com>
2019-03-20 19:21:31 -0300

    Displaying user's tweets

--- android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/view/TweetsListActivity.kt
+++ android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/view/TweetsListActivity.kt
@@ -1,19 +1,35 @@
 package br.com.sibela.tweetmood.view
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.LinearLayoutManager
 import br.com.sibela.tweetmood.R
+import br.com.sibela.tweetmood.adapter.TweetsAdapter
 import br.com.sibela.tweetmood.model.Tweet
+import kotlinx.android.synthetic.main.activity_tweets_list.*
 
-class TweetsListActivity : AppCompatActivity() {
+class TweetsListActivity : AppCompatActivity(), TweetsAdapter.Callback {
+
+    private lateinit var tweetsAdapter: TweetsAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_tweets_list)
 
         val tweets: ArrayList<Tweet> = intent.getParcelableArrayListExtra(TWEETS_DATA_KEY)
-        Toast.makeText(this, "", Toast.LENGTH_SHORT).show()
+        setupRecycler(tweets)
+    }
+
+    private fun setupRecycler(tweets: ArrayList<Tweet>) {
+        tweetsRecycler.setHasFixedSize(true)
+        tweetsRecycler.layoutManager = LinearLayoutManager(tweetsRecycler.context)
+        tweetsRecycler.addItemDecoration(CardSpacingItemDecoration(this, R.dimen.default_margin_between_elements))
+        tweetsAdapter = TweetsAdapter(tweets, this)
+        tweetsRecycler.adapter = tweetsAdapter
+    }
+
+    override fun displayFeeling(tweet: Tweet) {
+        TODO("not implemented")
     }
 
     companion object {
