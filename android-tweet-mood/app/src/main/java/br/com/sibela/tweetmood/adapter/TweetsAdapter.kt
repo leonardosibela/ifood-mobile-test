00e47765df5462210e514a464848bd66271a8191
leonardosibela <leonardo_sibela@hotmail.com>
2019-03-20 19:01:33 -0300

    Implementing adapter for Tweets list

---
+++ android-tweet-mood/app/src/main/java/br/com/sibela/tweetmood/adapter/TweetsAdapter.kt
@@ -0,0 +1,46 @@
+package br.com.sibela.tweetmood.adapter
+
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import br.com.sibela.tweetmood.R
+import br.com.sibela.tweetmood.model.Tweet
+import kotlinx.android.synthetic.main.tweet_card.view.*
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.collections.ArrayList
+
+class TweetsAdapter(val tweets: ArrayList<Tweet>, private val callback: TweetsAdapter.Callback) :
+    RecyclerView.Adapter<TweetsAdapter.TweetsViewHolder>() {
+
+    override fun onCreateViewHolder(viewGroup: ViewGroup, viewType: Int): TweetsViewHolder {
+        val view = LayoutInflater.from(viewGroup.context).inflate(R.layout.tweet_card, viewGroup, false)
+        return TweetsViewHolder(view)
+    }
+
+    override fun getItemCount(): Int {
+        return tweets.size
+    }
+
+    override fun onBindViewHolder(holder: TweetsViewHolder, position: Int) {
+        val tweet = tweets[position]
+        holder.timestamp.text = SimpleDateFormat(DISPLAY_DATE_FORMAT, Locale.ENGLISH).format(tweet.date).capitalize()
+        holder.tweetContent.text = tweet.text
+        holder.analyzeFeeling.setOnClickListener { callback.displayFeeling(tweet) }
+    }
+
+    class TweetsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        val tweetContent = itemView.tweetContent!!
+        val timestamp = itemView.timestamp!!
+        val analyzeFeeling = itemView.analyzeFeeling!!
+    }
+
+    interface Callback {
+        fun displayFeeling(tweet: Tweet)
+    }
+
+    companion object {
+        private const val DISPLAY_DATE_FORMAT = "MMM dd, yyyy - hh:mmaaa"
+    }
+}
